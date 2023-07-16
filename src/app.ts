df2af29311ae817a9de2c346533b0a5b97dc6bfb
h3 <joaquinbastias321@gmail.com>
2023-07-16 02:15:28 -0300

    Guardar audios

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import qrcode from 'qrcode-terminal';
 import { Client, LocalAuth, Message } from 'whatsapp-web.js';
 import { WhatsappMessage } from './interfaces/WhatsappMessage';
+import fs from 'fs'
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -14,9 +15,20 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', (message)=>{
-    const msg = message.rawData as WhatsappMessage
-    console.log(msg.stickerSentTs)
+client.on('message', async(message) => {
+    // AUDIO = 'audio',
+    // VOICE = 'ptt',
+    console.log('type',message.type);
+    if(message.type !== 'ptt')return
+    var mediaBuffer = await message.downloadMedia();
+
+    console.log(typeof mediaBuffer.data, typeof Buffer.from(mediaBuffer.data, 'base64'))
+
+    const realBuffer = Buffer.from(mediaBuffer.data, 'base64')
+    
+
+    fs.writeFileSync(`${message.author||'USER'}.ogg`, realBuffer);
+    // console.log('FILE:', `audio_${message.id}.png`);
 })
 
 client.initialize();
