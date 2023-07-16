dbca593e7cae4c9472fed2b4627d28d1e3caa698
h3 <joaquinbastias321@gmail.com>
2023-07-16 03:30:45 -0300

    sas

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import { Client, LocalAuth, Message } from 'whatsapp-web.js';
 import { WhatsappMessage } from './interfaces/WhatsappMessage';
 import fs from 'fs'
 
+import say from 'say'
+
+
+
 const client = new Client({
     authStrategy: new LocalAuth()
 });
@@ -19,16 +23,18 @@ client.on('message', async(message) => {
     // AUDIO = 'audio',
     // VOICE = 'ptt',
     console.log('type',message.type);
-    if(message.type !== 'ptt')return
-    var mediaBuffer = await message.downloadMedia();
+    if(message.type === 'ptt'){
+        var mediaBuffer = await message.downloadMedia();
+        console.log(typeof mediaBuffer.data, typeof Buffer.from(mediaBuffer.data, 'base64'))
+        const realBuffer = Buffer.from(mediaBuffer.data, 'base64')
+        fs.writeFileSync(`${message.author||'USER'}.ogg`, realBuffer);
+    }
+    if(message.type === 'chat' && message.body.startsWith('!')){
+        say.speak(message.body, undefined, 1)
+    }
 
-    console.log(typeof mediaBuffer.data, typeof Buffer.from(mediaBuffer.data, 'base64'))
-
-    const realBuffer = Buffer.from(mediaBuffer.data, 'base64')
     
 
-    fs.writeFileSync(`${message.author||'USER'}.ogg`, realBuffer);
-    // console.log('FILE:', `audio_${message.id}.png`);
 })
 
 client.initialize();
