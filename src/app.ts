42bb75f8de88e7f03c1b4430efac8de237074955
dotH3 <joaquinbastias321@gmail.com>
2024-04-27 23:46:32 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
 import qrcode from 'qrcode-terminal';
-import { Client, LocalAuth, Message } from 'whatsapp-web.js';
 import { WhatsappMessage } from './interfaces/WhatsappMessage';
-import fs from 'fs'
+const { Client } = require('whatsapp-web.js');
 
-import say from 'say'
-
-
-
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
+const client = new Client();
 
 client.on('qr', (qr: string) => {
     qrcode.generate(qr, { small: true });
@@ -19,22 +12,8 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', async(message) => {
-    // AUDIO = 'audio',
-    // VOICE = 'ptt',
-    console.log('type',message.type);
-    if(message.type === 'ptt'){
-        var mediaBuffer = await message.downloadMedia();
-        console.log(typeof mediaBuffer.data, typeof Buffer.from(mediaBuffer.data, 'base64'))
-        const realBuffer = Buffer.from(mediaBuffer.data, 'base64')
-        fs.writeFileSync(`${message.author||'USER'}.ogg`, realBuffer);
-    }
-    if(message.type === 'chat' && message.body.startsWith('!')){
-        say.speak(message.body, undefined, 1)
-    }
-
-    
-
+client.on('message', (message:WhatsappMessage) => {
+    console.log('hello');
 })
 
 client.initialize();
